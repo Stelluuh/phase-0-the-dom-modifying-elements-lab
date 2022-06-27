8e558e3d69339b7370b44e335f01420620d9edc9
stellamunson <sftamesis13@gmail.com>
2022-06-27 14:09:31 -0700

    commit tree 4373ab69a1d4e71929077f6a8bd0767a20d86c3d

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+// 1) no longer has DOM node 'main#main'
+document.getElementById('main').remove()
+
+// 2) has a 'newHeader' variable that points to an <h1> node, 'h1#victory'
+const newHeader = document.createElement('h1')
+
+
+// 3) the 'newHeader' variable points to a node that has an id of 'victory'
+newHeader.id = 'victory'
+
+// 4) the 'newHeader' variable points to a node 'h1#victory', has "YOUR-NAME 
+//is the champion" inside.
+
+newHeader.textContent = "YOUR-NAME is the champion"
